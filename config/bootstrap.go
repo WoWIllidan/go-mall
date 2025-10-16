12bf344936a7d1fd6f3d4a153ba918bd3fe5c75c
WoWBytePaladin <helloworldgnu@gmail.com>
2025-10-17 00:13:33 +0800

    feat: Add configuration

---
+++ config/bootstrap.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+//go:embed *.yaml
+var configs embed.FS
+
+func init() {
+	env := os.Getenv("ENV")
+	if env == "" {
+		panic("env variable not set")
+	}
+
+	vp := viper.New()
+
+	configFileStream, err := configs.ReadFile("application." + env + ".yaml")
+	fmt.Println("configFileStream", string(configFileStream))
+
+	if err != nil {
+		panic(err)
+	}
+
+	vp.SetConfigType("yaml")
+	err = vp.ReadConfig(bytes.NewReader(configFileStream))
+	if err != nil {
+		panic(err)
+	}
+
+	err = vp.UnmarshalKey("app", &App)
+	if err != nil {
+		panic(err)
+	}
+
+	err = vp.UnmarshalKey("database", &Database)
+	if err != nil {
+		panic(err)
+	}
+
+	Database.MaxLifeTime *= time.Second
+}
--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/WoWBytePaladin/go-mall/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/config-read", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"type":     config.Database.Type,
+			"max_life": config.Database.MaxLifeTime,
 		})
 	})
 
