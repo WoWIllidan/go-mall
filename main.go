2fd9a839356e5161922d4ce3f5a94b79c1a4a606
WoWBytePaladin <helloworldgnu@gmail.com>
2025-10-17 23:41:19 +0800

    feat: Go Log Facade - Simplifies Zap operations, automatically injects traceId and code location

--- main.go
+++ main.go
@@ -3,21 +3,35 @@ package main
 import (
 	"net/http"
 
+	"github.com/WoWBytePaladin/go-mall/common/logger"
+	"github.com/WoWBytePaladin/go-mall/common/middleware"
 	"github.com/WoWBytePaladin/go-mall/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/config-read", func(c *gin.Context) {
+	g := gin.New()
+
+	g.Use(gin.Logger(), middleware.StartTrace())
+	g.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"type":     config.Database.Type,
-			"max_life": config.Database.MaxLifeTime,
+			"message": "pong",
 		})
 	})
 
-	err := r.Run(":8080")
-	if err != nil {
-		return
-	}
+	g.GET("/config-read", func(c *gin.Context) {
+		database := config.Database
+		c.JSON(http.StatusOK, gin.H{
+			"type":     database.Type,
+			"max_life": database.MaxLifeTime,
+		})
+	})
+
+	g.GET("/logger-test", func(c *gin.Context) {
+		logger.New(c).Info("logger test", "key", "keyName", "val", 2)
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+	g.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
