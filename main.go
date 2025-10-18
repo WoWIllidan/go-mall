44d87ac7ce9b44084f2241c3fe0c11fb4e0a0e21
WoWBytePaladin <helloworldgnu@gmail.com>
2025-10-18 13:18:58 +0800

    feat: Define project Error to implement error chain and record the occurrence location

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/WoWBytePaladin/go-mall/common/errcode"
 	"github.com/WoWBytePaladin/go-mall/common/logger"
 	"github.com/WoWBytePaladin/go-mall/common/middleware"
 	"github.com/WoWBytePaladin/go-mall/config"
@@ -51,6 +53,25 @@ func main() {
 		})
 	})
 
-	g.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	g.GET("/customized-error-test", func(c *gin.Context) {
 
+		// 使用 Wrap 包装原因error 生成 项目error
+		err := errors.New("a dao error")
+		appErr := errcode.Wrap("包装错误", err)
+		bAppErr := errcode.Wrap("再包装错误", appErr)
+		logger.New(c).Error("记录错误", "err", bAppErr)
+
+		// 预定义的ErrServer, 给其追加错误原因的error
+		err = errors.New("a domain error")
+		apiErr := errcode.ErrServer.WithCause(err)
+		logger.New(c).Error("API执行中出现错误", "err", apiErr)
+
+		c.JSON(apiErr.HttpStatusCode(), gin.H{
+			"code": apiErr.Code(),
+			"msg":  apiErr.Msg(),
+		})
+
+	})
+
+	g.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
